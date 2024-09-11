d61aba98d8270ae97dbbc048da99e756b91b42f6
meizibiepao <89424436@qq.com>
2024-09-11 11:31:26 +0800

    Remove unnecessary references to my-website

--- web3/js/walletFunctions.js
+++ web3/js/walletFunctions.js
@@ -259,7 +259,7 @@ async function addTestToken() {
                             address: '0x92f648689ec1187c20619951eea24c8f5752ebf4',
                             symbol: 'MCFTS',
                             decimals: 18,
-                            image: 'https://meizibiepao.github.io/my-website/images/tokenIcon/CatEarCoin.png',
+                            image: 'https://meizibiepao.github.io/idle_fantasy/images/tokenIcon/CatEarCoin.png',
                         },
                     },
                 });
